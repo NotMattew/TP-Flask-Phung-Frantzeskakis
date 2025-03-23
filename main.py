b50fc403daceeec8cf9586038ea085c7392ca4a6
Mattew <139057272+NotMattew@users.noreply.github.com>
2025-03-23 16:53:28 +0100

    big changes
    les produits deviennent les personnes
    la fin arrive

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ babysitter = {
         "age" : 21,
         "genre" : "Femme",
         "qualite" : ["Motivée", "Énergétique", "Bienveillante"],
-        "domaine" : [""]
+        "domaine" : ["Maisons"]
     },
     "b_aitana" : {
         "pic" : "/images/b_aitana.jpg",
@@ -16,71 +16,71 @@ babysitter = {
         "age" : 27,
         "genre" : "Femme",
         "qualite" : ["Sportive", "Motivante", "Coopérative"],
-        "domaine" : [""]
+        "domaine" : ["Écoles", "Sorties"]
     },
     "c_marion" : {
-        "pic" : "/images/",
+        "pic" : "/images/c_marion.jpg",
         "nom" : "Marion C",
         "age" : 23,
         "genre" : "Femme",
-        "qualite" : [""],
-        "domaine" : [""]
+        "qualite" : ["Créative", "Curieuse", "Attentive"],
+        "domaine" : ["Maisons"]
     },
     "f_thomas" : {
-        "pic" : "/images/",
+        "pic" : "/images/f_thomas.jpg",
         "nom" : "Thomas F",
         "age" : 25,
         "genre" : "Homme",
         "qualite" : ["Assidû", "Calme", "Prévoyant"],
-        "domaine" : ["Écoles", "Sorties scolaires"]
+        "domaine" : ["Écoles", "Sorties", "Anniversaires"]
     },
     "h_thierry" : {
-        "pic" : "/images/",
+        "pic" : "/images/f_thomas.jpg",
         "nom" : "Thierry H",
         "age" : 47,
         "genre" : "Homme",
-        "qualite" : [""],
-        "domaine" : [""]
+        "qualite" : ["Logique", "Réfléchi", "Déterminé"],
+        "domaine" : ["Écoles", "Sorties"]
     },
     "ii_elizabeth" : {
-        "pic" : "/images/",
+        "pic" : "/images/f_thomas.jpg",
         "nom" : "Elizabeth II",
         "age" : 98,
         "genre" : "Femme",
-        "qualite" : [""],
-        "domaine" : [""]
+        "qualite" : ["Tolérante", "Sage", "Fiable"],
+        "domaine" : ["Maisons", "Écoles","Anniversaires", "Sorties"]
     },
     "m_julie" : {
-        "pic" : "/images/",
+        "pic" : "/images/m_julie.jpg",
         "nom" : "Julie M",
         "age" : 19,
         "genre" : "Femme",
-        "qualite" : [""],
-        "domaine" : [""]
+        "qualite" : ["Ambitieuse", "Enthousiaste", "Ponctuelle"],
+        "domaine" : ["Maisons"]
     },
     "p_brad" : {
-        "pic" : "/images/",
+        "pic" : "/images/p_brad.jpg",
         "nom" : "Brad P",
         "age" : 61,
         "genre" : "Homme",
-        "qualite" : [""],
-        "domaine" : [""]
+        "qualite" : ["Honnête", "Modeste", "Généreux"],
+        "domaine" : ["Écoles", "Anniversaires"]
     },
     "v_blanche" : {
-        "pic" : "/images/",
+        "pic" : "/images/v_blanche.jpg",
         "nom" : "Blanche V",
         "age" : 32,
         "genre" : "Homme",
-        "qualite" : [""],
-        "domaine" : [""]
+        "qualite" : ["Sociable", "Souriante", "Organisée"],
+        "domaine" : ["Maisons"]
     },
     "z_zoe" : {
-        "pic" : "/images/",
+        "pic" : "/images/z_zoe.jpg",
         "nom" : "Zoé Z",
         "age" : 31,
         "genre" : "Femme",
-        "qualite" : [""],
-        "domaine" : [""]
+        "qualite" : ["Adaptable", "Positive", "Innovante"],
+        "domaine" : ["Maisons", "Sorties"]
     }
 }
 
@@ -88,7 +88,7 @@ babysitter = {
 @app.route("/")
 def hello():
     return render_template(
-        "home.html"
+        "home.html", babysitter = babysitter
     )
 
 
